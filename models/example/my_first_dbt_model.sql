42f643e69fc5bbcbfeb05ee266d0725509bc94c2
Japheth Rutoh <jruhtz@gmail.com>
2024-03-29 20:09:20 +0000

    fixed null test in my_first_dbt_model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -12,7 +12,7 @@ with source_data as (
 
     select 1 as id
     union all
-    select 12 as id
+    select null as id
 
 )
 
@@ -23,4 +23,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+where id is not null
